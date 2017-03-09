328abd18636cddb84a55103c2222e168d428375d
einarzb <einarzb@gmail.com>
2017-03-09 14:24:42 +0200

    added port to server 8000 and get

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var app = express();
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+app.get('/beer', function(req, res) {
+  console.log("beer");  
+});
+
 app.listen(8000, function() {
-  console.log("Fullstack project. Listening on 8000.")
+  console.log("beer list project. Listening on 8000.")
 
 });
