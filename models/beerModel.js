8f52bd03cbddc3827f8a0fa3b277ba91cb26c0c7
einarzb <einarzb@gmail.com>
2017-03-12 09:03:06 +0200

    some changes

--- models/beerModel.js
+++ models/beerModel.js
@@ -2,24 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema; //the function that constructs objects
 
 var beerSchema = new Schema ({ 
-  name: String,
-  style: String,
-  image:String,
-  abv: Number
+  name: {type:String},
+  style: {type:String},
+  image:{type:String},
+  abv: {type:Number},
+  rating: {type:Number},
+  averageRating: {type:Number}
 });
 
 var Beer = mongoose.model('BeerModel', beerSchema); 
 module.exports = Beer;
 //when we require this file we want to return the object that was assigned to module.export.
 
-// var goldstar = new Beer ({
-//   name: "goldstar" ,
-//   style: "malt",
-//   image: "http://bit.ly/1XtmB4d",
-//   abv: 4.5
-// });
-
-// goldstar.save(function(err, res){
-//   if(err){console.log(err);}
-//   console.log(res);
-// });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/beerslist');
-var Beer = require("./models/BeerModel")
+var Beer = require("./models/beerModel")
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -21,8 +21,8 @@ app.get('/beers', function(req, res, next) {
 
   res.json({
     beers: [
-        { name: '512 IPA', style: 'IPA', image: 'http://bit.ly/1XtmB4d', abv: 5 },
-        { name: '512 Pecan Porter', style: 'Porter', image: 'http://bit.ly/1Vk5xj4', abv: 4 }
+        { name: '512 IPA', style: 'IPA', image: 'http://bit.ly/1XtmB4d', abv: 5, rating: 5, averageRating: 5},
+        { name: '512 Pecan Porter', style: 'Porter', image: 'http://bit.ly/1Vk5xj4', abv: 4, rating: 3, averageRating: 2}
   ]});
 });
 
